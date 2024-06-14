1c83817de2ff7b2755193e8dba5fed2d4d78f4f8
RIVAL231 <sankalpsharma250@gmail.com>
2024-06-15 02:33:50 +0530

    Initial commit

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className="header">
             <img 
-                src="./Components/troll-face.png" 
+                src="https://upload.wikimedia.org/wikipedia/en/9/9a/Trollface_non-free.png" 
                 className="header--image"
             />
             <h2 className="header--title">Meme Generator</h2>
